2e80ec94f31f3d3d2517021fa062c1cf7f6ee61e
lasse <77295879+lassv@users.noreply.github.com>
2022-12-27 18:15:35 +0000

    Check nitro with discord API

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 var prompt = require("prompt-sync")();
-var axios = require("axios");
+var fetch = require("node-fetch");
 var id = require("yourid");
 var chalk = require("chalk");
+var fs = require("fs");
+var filename = "nitros.txt";
+var API_END_POINT_START =
+  "https://discordapp.com/api/v8/entitelemnts/gift-codes";
 let count;
 
 const countOfChecks = prompt(
@@ -13,3 +17,66 @@ if (countOfChecks) {
 } else {
   count = 300;
 }
+
+// Check Lines
+async function checkLines() {
+  if (!fs.existsSync(filename)) {
+    console.log(chalk.red("The nitros file does not exist!"));
+  } else {
+    fs.readFile(filename, "utf8", (err, data) => {
+      if (err) throw err;
+
+      const lines = data.split("\n");
+      lines.forEach(async (line) => {
+        fetch(`${API_END_POINT_START}/${line}`)
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.code === 0) {
+              console.log(
+                chalk.red("Invalid Code Found > ") +
+                  chalk.gray(`https://discord.gift/${line}`)
+              );
+            } else {
+              console.log(
+                chalk.green("Valid Code Found > ") +
+                  chalk.gray(`https://discord.gift/${line}`)
+              );
+            }
+          })
+          .catch((err) => {
+            console.log(chalk.red("Error: " + err.message));
+          });
+      });
+    });
+  }
+}
+
+// Write lines
+function writeCountToFile(lines) {
+  if (typeof lines !== "number") {
+    console.log(chalk.red("The lines count must be a number."));
+  } else {
+    if (fs.existsSync(filename)) {
+      console.log(
+        chalk.red("Nitro file already exists, delete it to make new nitros!")
+      );
+    } else {
+      let line = "";
+      for (let i = 0; i < count; i++) {
+        line += `${id.generate({ length: 16 })}\n`;
+      }
+
+      fs.writeFile(filename, line, (err) => {
+        if (err) throw err;
+        console.log(
+          chalk.green("The nitro links has been saved to: ") +
+            chalk.gray("nitros.txt")
+        );
+
+        checkLines();
+      });
+    }
+  }
+}
+
+writeCountToFile(count);
